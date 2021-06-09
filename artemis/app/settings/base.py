ccfd6c78370674be3254628e449a92aa90d5bb31
Michelle Yung <myung@email.arizona.edu>
2021-06-09 09:36:38 -0700

    add local settings file

--- artemis/app/settings/base.py
+++ artemis/app/settings/base.py
@@ -15,19 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ugj78k&%s%!wf7@343dfs!xz)c78w%slc5u3c6)@4pb^9^xla_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -68,17 +55,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'artemis.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
