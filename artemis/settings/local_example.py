2701d272bcc08fc58e1bbd74584fe58edfbb5129
Michelle Yung <myung@email.arizona.edu>
2021-06-16 09:56:58 -0700

    update local_example with postgres config

--- artemis/settings/local_example.py
+++ artemis/settings/local_example.py
@@ -16,7 +16,11 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'artemis',
+        'USER': 'artemis',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
